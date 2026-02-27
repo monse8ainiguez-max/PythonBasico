1d27d83a4935d0c3b55d6208e237563c8e481324
monse8ainiguez-max <monse8ainiguez@gmail.com>
2026-02-26 17:03:15 -0800

    creamos sesion_01

---
+++ sesion_01.py
@@ -0,0 +1,45 @@
+# Numeros
+print(int(7))
+print(float(7.7))
+print(type(7))
+print(type(7.7))
+print(int(1+2))
+print(int(10*2))
+print(int(1 + 4 - 2))
+print(float(1+2.0))
+
+# Operadores matematicos
+# +
+# -
+# *
+# /
+# **
+# % Modulo
+
+print(int(2**3))
+print(int(4**8))
+print(float(10%3))
+print(float(25%4))
+print(float(16%2))
+print(float(10 / 3))
+
+# Variables
+print("==Variables==")
+x = 100
+y = 1
+print(x+y)
+ventas = 1999991
+print("nuestras ventas fueron",ventas)
+
+is_active = True
+print(is_active)
+
+game_over = False
+print(game_over)
+some_string = "hola soy un string"
+print(some_string)
+
+print("==condicionales==")
+edad = 18
+if (edad >= 18):
+    print("Si puedes entrar en el bar")
